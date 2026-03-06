c1a4cf278ca4c4e49e321989ffd309820ee5e51c
mandulmj-cpu <mandulmj@gmail.com>
2026-03-06 11:25:54 +0900

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+/**
+ * @license
+ * SPDX-License-Identifier: Apache-2.0
+ */
+import MyRealTripPerfectFinal from './components/MyRealTripPerfectFinal';
+
+export default function App() {
+  return <MyRealTripPerfectFinal />;
+}
